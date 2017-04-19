e5c1c1ad1d3b562fbb2e396811d6af1fa8b84994
G Pro <G Pro@MAQUINA1>
2017-04-19 11:19:50 -0500

    inicio

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -18,47 +18,47 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner tecla=new Scanner(System.in);
-        int p;
-        System.out.println("**UNIVERSIDAD TÉCNICA DE AMBATO");
-        System.out.println("BIENVENIDOS ");
-        System.out.println("Integrantes :");
-        System.out.println("Dennis Barba");
-        System.out.println("Iveth Barros");
-        System.out.println("Marilyn Barros");
-        System.out.println("MonserrathFrutos");
-        System.out.println("Cristina Nuñez");
-
-        System.out.println(" Operaciones Básicas ");
-        System.out.println("1- SUMA");
-         System.out.println("2- RESTA");
-          System.out.println("3- MULTIPLICACIÓN");
-           System.out.println("4-DIVISIÓN");
-            System.out.println("5- RAIZ CUADRADA");
-            
-            System.out.println("Escoja una Opción :");
-            p=tecla.nextInt();
-            
-            if(p==0 || p>5){
-                System.out.println("No existe dicha opciñon");
-            }else if(p==1){
-                Suma1  suma= new Suma1();
-                suma.operacion();
-            }else if(p==2){
-               Resta resta= new Resta();
-               resta.Resta();
-            }else if(p==3){
-                Multiplicacion mul=new Multiplicacion();
-                mul.multiplicar();
-            }else if(p==4){
-               Division div = new Division();
-               div.div();
-                
-            }else if(p==5){
-              raiz rcd=new raiz();
-             rcd.rc();
-                
-            }
+//        Scanner tecla=new Scanner(System.in);
+//        int p;
+//        System.out.println("**UNIVERSIDAD TÉCNICA DE AMBATO");
+//        System.out.println("BIENVENIDOS ");
+//        System.out.println("Integrantes :");
+//        System.out.println("Dennis Barba");
+//        System.out.println("Iveth Barros");
+//        System.out.println("Marilyn Barros");
+//        System.out.println("MonserrathFrutos");
+//        System.out.println("Cristina Nuñez");
+//
+//        System.out.println(" Operaciones Básicas ");
+//        System.out.println("1- SUMA");
+//         System.out.println("2- RESTA");
+//          System.out.println("3- MULTIPLICACIÓN");
+//           System.out.println("4-DIVISIÓN");
+//            System.out.println("5- RAIZ CUADRADA");
+//            
+//            System.out.println("Escoja una Opción :");
+//            p=tecla.nextInt();
+//            
+//            if(p==0 || p>5){
+//                System.out.println("No existe dicha opciñon");
+//            }else if(p==1){
+//                Suma1  suma= new Suma1();
+//                suma.operacion();
+//            }else if(p==2){
+//               Resta resta= new Resta();
+//               resta.Resta();
+//            }else if(p==3){
+//                Multiplicacion mul=new Multiplicacion();
+//                mul.multiplicar();
+//            }else if(p==4){
+//               Division div = new Division();
+//               div.div();
+//                
+//            }else if(p==5){
+//              raiz rcd=new raiz();
+//             rcd.rc();
+//                
+//            }
             
             
             
